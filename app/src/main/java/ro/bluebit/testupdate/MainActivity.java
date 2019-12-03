5f2adc108e3182a0cd6a92408e5ae5c6f2ea1ee3
Adrian <knifeofficial@gmail.com>
2019-12-03 16:21:16 +0200

    varianta xml 8

--- app/src/main/java/ro/bluebit/testupdate/MainActivity.java
+++ app/src/main/java/ro/bluebit/testupdate/MainActivity.java
@@ -3,9 +3,12 @@ package ro.bluebit.testupdate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
 import com.github.javiersantos.appupdater.enums.Display;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
 import com.github.javiersantos.appupdater.objects.Update;
@@ -19,9 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         versiune=findViewById(R.id.versiune_id);
-        UpdateVersiune();
+
+        UpdateVers2();
     }
 
+
+    public void UpdateVers2(){
+        AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(this)
+                .setUpdateFrom(UpdateFrom.XML)
+                .setGitHubUserAndRepo("AdrianDamoc","TestareUpdate")
+                .setUpdateXML("https://raw.githubusercontent.com/AdrianDamoc/TestareUpdate/master/app/update.xml")
+                //...
+                .withListener(new AppUpdaterUtils.UpdateListener() {
+                    @Override
+                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+                        Log.d("Latest Version", update.getLatestVersion());
+                        Log.d("Latest Version Code", String.valueOf(update.getLatestVersionCode()));
+                        Log.d("Release notes", update.getReleaseNotes());
+                        Log.d("URL", String.valueOf(update.getUrlToDownload()));
+                        Log.d("Is update available?", Boolean.toString(isUpdateAvailable));
+                    }
+
+                    @Override
+                    public void onFailed(AppUpdaterError error) {
+                        Log.d("AppUpdater Error", "Something went wrong");
+                    }
+                });
+        appUpdaterUtils.start();
+    }
     public void UpdateVersiune(){
         AppUpdater appUpdater = new AppUpdater(this)
                 .setUpdateFrom(UpdateFrom.XML)
