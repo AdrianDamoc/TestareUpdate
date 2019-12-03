a923732c296adf4a29a945c039a2f179eaa2374a
Adrian <knifeofficial@gmail.com>
2019-12-03 15:22:47 +0200

    Versiunea 3

--- app/src/main/java/ro/bluebit/testupdate/MainActivity.java
+++ app/src/main/java/ro/bluebit/testupdate/MainActivity.java
@@ -3,6 +3,7 @@ package ro.bluebit.testupdate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.Display;
@@ -10,16 +11,18 @@ import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView versiune;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        AppUpdater appUpdater = new AppUpdater(this)
-                .setUpdateFrom(UpdateFrom.GITHUB)
-                .setGitHubUserAndRepo("AdrianDamoc", "TestareUpdate")
-                .setTitleOnUpdateAvailable("Update available")
-                .setDisplay(Display.NOTIFICATION);
+        versiune=findViewById(R.id.versiune_id);
+        AppUpdater appUpdater = new AppUpdater(this);
+        appUpdater.setUpdateFrom(UpdateFrom.GITHUB);
+        appUpdater.setGitHubUserAndRepo("AdrianDamoc", "TestareUpdate");
+        appUpdater.setTitleOnUpdateAvailable("Update available");
+        appUpdater.setDisplay(Display.NOTIFICATION);
         appUpdater.start();
     }
 }
